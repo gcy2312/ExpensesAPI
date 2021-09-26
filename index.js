bd21de8df7ccf27ed35f36512db781444ae91746
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-09-26 16:59:33 -0400

    logic for endpoints (tested with postman)

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const uuid = require('uuid');
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+
+const Bills = Models.Bill;
+const Expenses = Models.Expense;
+const Users = Models.User;
+
+mongoose.connect('mongodb://localhost:27017/billFoldDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 app.use(morgan('common'));
 app.use(express.static('public'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 //documentation request
@@ -19,63 +28,254 @@ app.get('/', (req, res) => {
   res.send('welcome to BillFold');
 });
 
-app.get('/users/:userId', (req, res) => {
-  res.send('get user by ID');
+//get user by userId
+app.get('/users/:id', (req, res) => {
+  // Users.find({ _id: req.params.userId })
+  Users.findById(req.params.id)
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
-app.get('/user/:userId/expenses', (req, res) => {
-  res.send('list of all expenses for user');
+//get users list of expenses
+app.get('/users/:id/expenses', (req, res) => {
+  Expenses.find({ UserId: req.params.id })
+    .then((userExpenses) => {
+      res.json(userExpenses);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
-app.get('/users/:userId/bills', (req, res) => {
-  res.send('list of all bills for user');
+//get users list of bills
+app.get('/users/:id/bills', (req, res) => {
+  Bills.find({ UserId: req.params.id })
+    .then((bills) => {
+      res.json(bills);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
+//get single expense info
 app.get('/expenses/:expenseId', (req, res) => {
-  res.send('single expense info');
+  Expenses.find({ _id: req.params.expenseId })
+    .then((expense) => {
+      res.json(expense);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
+//get single bill info
 app.get('/bills/:billId', (req, res) => {
-  res.send('single bill info');
+  Bills.find({ _id: req.params.billId })
+    .then((bill) => {
+      res.json(bill);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
 //POST requests (create)
+
+//regsiter new user
 app.post('/users', (req, res) => {
-  res.send('create new user');
+  Users.findOne({ Email: req.body.Email })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Email + 'is already registered');
+      } else {
+        Users
+          .create({
+            FirstName: req.body.FirstName,
+            LastName: req.body.LastName,
+            Username: req.body.Username,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            CurrencyPref: req.body.CurrencyPref
+          })
+          .then((user) => { res.status(201).json(user) })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+          })
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
 });
 
-app.post('/users/:userId/expenses', (req, res) => {
-  res.send('create new expense');
+//create new expense doc
+app.post('/users/:id/expenses', (req, res) => {
+  Expenses.create({
+    ...req.body, UserId: req.params.id, Index: true
+  })
+    .then((expense) => { res.status(201).json(expense) })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
 });
 
-app.post('/users/:userId/bills', (req, res) => {
-  res.send('create new bill');
+//create new bill doc
+app.post('/users/:id/bills', (req, res) => {
+  Bills.create({
+    ...req.body, UserId: req.params.id, Index: true
+  })
+    .then((bill) => { res.status(201).json(bill) })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
 });
 
 //PUT requests (update)
-app.put('/users/:userId', (req, res) => {
-  res.send('Successful PUT request updating data of user');
+
+//update user by id
+app.put('/users/:id', (req, res) => {
+  Users.findByIdAndUpdate(req.params.id, {
+    $set:
+    {
+      FirstName: req.body.FistName,
+      LastName: req.body.LastName,
+      Username: req.body.Username,
+      Password: req.body.Password,
+      Email: req.body.Email,
+      CurrencyPref: req.body.CurrencyPref
+    }
+  },
+    { new: true }, // This line makes sure that the updated document is returned
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      } else {
+        res.json(updatedUser);
+      }
+    });
 });
 
-app.put('/expenses/:expenseId', (req, res) => {
-  res.send('update expense');
+//update single expense doc
+app.put('/users/:id/expenses/:expenseId', (req, res) => {
+  Expenses.findByIdAndUpdate({ _id: req.params.expenseId }, {
+    $set:
+    {
+      Category: req.body.Category,
+      Description: req.body.Description,
+      Date: req.body.Date,
+      Amount: req.body.Amount,
+      Currency: req.body.Currency,
+      UserId: req.params.id,
+      Index: true
+    }
+  },
+    { new: true }, // This line makes sure that the updated document is returned
+    (err, updatedExpense) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      } else {
+        res.json(updatedExpense);
+      }
+    });
 });
 
-app.put('/bills/:billId', (req, res) => {
-  res.send('update bill');
+//update single bill doc
+app.put('/users/:id/bills/:billId', (req, res) => {
+  Bills.findByIdAndUpdate({ _id: req.params.billId }, {
+    $set:
+    {
+      Description: req.body.Description,
+      Date: req.body.Date,
+      Amount: req.body.Amount,
+      Currency: req.body.Currency,
+      UserId: req.params.id,
+      Paid: req.body.Paid,
+      Index: true
+    }
+  },
+    { new: true }, // This line makes sure that the updated document is returned
+    (err, updatedBill) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      } else {
+        res.json(updatedBill);
+      }
+    });
 });
 
 //DELETE requests
-app.delete('/users/:userId', (req, res) => {
-  res.send('delete user');
+
+//delete user account
+app.delete('/users/:id', (req, res) => {
+  Users.findByIdAndRemove(req.params.id)
+    .then((user) => {
+      if (!user) {
+        res.status(400).send('Account was not found');
+      } else {
+        res.status(200).send('Account has been successfully deleted.');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
+//delete expense by id
 app.delete('/expenses/:expenseId', (req, res) => {
-  res.send('delete expense');
+  Expenses.findByIdAndRemove({ _id: req.params.expenseId })
+    .then((expense) => {
+      if (!expense) {
+        res.status(400).send('Expense item was not found');
+      } else {
+        res.status(200).send('Expense item has been successfully deleted.');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
+//delete bill by id
 app.delete('/bills/:billId', (req, res) => {
-  res.send('delete bill');
+  Bills.findByIdAndRemove({ _id: req.params.billId })
+    .then((bill) => {
+      if (!bill) {
+        res.status(400).send('Bill item was not found');
+      } else {
+        res.status(200).send('Bill item has been successfully deleted.');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
 });
 
 
