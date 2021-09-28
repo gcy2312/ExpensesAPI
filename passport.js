c9331504895c7821007a56b5514a28bf531c636f
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2021-09-28 19:00:38 -0400

    add token (config vars heroku)

--- passport.js
+++ passport.js
@@ -35,7 +35,7 @@ passport.use(new LocalStrategy({
 
 passport.use(new JWTStrategy({
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-  secretOrKey: 'your_jwt_secret'
+  secretOrKey: process.env.JWT_TOKEN
 }, (jwtPayload, callback) => {
   return Users.findById(jwtPayload._id)
     .then((user) => {
