79396fff91952c542500b5b9102831536300a0cb
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2022-01-03 09:23:02 -0500

    hashedPassword for update password

--- index.js
+++ index.js
@@ -188,13 +188,14 @@ app.post('/users/:id/bills', passport.authenticate('jwt', { session: false }), (
 
 //update user by id
 app.put('/users/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  let hashedPassword = Users.hashPassword(req.body.Password);
   Users.findByIdAndUpdate(req.params.id, {
     $set:
     {
       FirstName: req.body.FirstName,
       LastName: req.body.LastName,
       Username: req.body.Username,
-      Password: req.body.Password,
+      Password: hashedPassword,
       Email: req.body.Email,
       CurrencyPref: req.body.CurrencyPref
     }
