155989f1ec73732deddd027a06f9fa6a369f7dc6
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2022-10-28 13:59:24 -0400

    use all origins

--- index.js
+++ index.js
@@ -22,19 +22,19 @@ const Users = Models.User;
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
-let allowedOrigins = ['http://localhost:58157', 'http://localhost:4200', 'https://gcy2312.github.io/billFold'];
+// let allowedOrigins = ['http://localhost:58157', 'http://localhost:4200', 'https://gcy2312.github.io/billFold'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
-// app.use(cors());
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//       return callback(new Error(message), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(morgan('common'));
 
