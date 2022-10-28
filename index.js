79a48c9ac9d8aaadb39e47a326e45bbe5f56fbaa
gcy2312 <78692599+gcy2312@users.noreply.github.com>
2022-10-28 14:14:32 -0400

    debug cors

--- index.js
+++ index.js
@@ -22,14 +22,14 @@ const Users = Models.User;
 mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
-const corsOptions = {
-  origin: '*',
-  credentials: true,            //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-}
+// const corsOptions = {
+//   origin: '*',
+//   credentials: true,            //access-control-allow-credentials:true
+//   optionSuccessStatus: 200,
+// }
 
-app.use(cors(corsOptions)) // Use this after the variable declaration
-// app.use(cors());
+// app.use(cors(corsOptions)) // Use this after the variable declaration
+app.use(cors());
 app.use(bodyParser.json());
 app.use(morgan('common'));
 
